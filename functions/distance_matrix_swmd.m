f2a687d0c72e82f2543fbc20fbf3db52762f3f90
Xu Zhang <spongezhang@gmail.com>
2017-01-30 21:41:05 -0500

    calculate distance

---
+++ functions/distance_matrix_swmd.m
@@ -0,0 +1,32 @@
+function [WMD] = distance_matrix_swmd(xtr, ytr, BOW_xtr, indices_tr, w, lambda, A)
+ntr = length(ytr);
+
+if ~exist('lambda', 'var') || isempty(lambda)
+    lambda = 1e-4;
+end
+
+if ~exist('A', 'var') || isempty(A)
+    A = eye(size(xtr{1},1));
+end
+
+WMD = zeros(ntr,ntr);
+parfor i = 1:ntr
+    disp([num2str(i) ' done']);
+    Wi = zeros(1,nte);
+    xi    = xtr{i};
+    bow_i = BOW_xtr{i}';
+    a = bow_i .*w(indices_tr{i});
+    a = a / sum(a);
+    for j = 1:ntr
+        xj    = xtr{j};
+        bow_j = BOW_xtr{j}';
+        b =bow_j.*w(indices_tr{j});
+        b = b / sum(b);
+        D  = distance(A*xi, A*xj);
+        D(D < 0) = 0;
+        D = full(D); 
+        [alpha, beta, T, dprimal, ddual] = sinkhorn(D, a, b, lambda, 200, 1e-3);
+        Wi(j) = dprimal;
+    end
+    WMD(i,:) = Wi;
+end
--- swmd_distance.m
+++ swmd_distance.m
@@ -21,7 +21,7 @@ lr_w = 1e+1; % learning rate for w
 lr_A = 1e+0;  % learing rate for A
 lambda = 10; % parameter in regularized transport problem (lambda in the paper)
 
-cv_folds = 5; % number of folds for cross-validation
+cv_folds = 1; % number of folds for cross-validation
 for split = 1:cv_folds
     save_couter = 0;
     Err_v = [];
@@ -29,19 +29,8 @@ for split = 1:cv_folds
     w_all = [];
     A_all = [];
     [xtr,xte,ytr,yte, BOW_xtr,BOW_xte, indices_tr, indices_te] = load_data(dataset, split);
-    [idx_tr, idx_val] = makesplits(ytr, 1-1/cv_folds, 1, 1);
 
-    xv = xtr(idx_val);
-    yv = ytr(idx_val);
-    BOW_xv = BOW_xtr(idx_val);
-    indices_v = indices_tr(idx_val);
-    xtr = xtr(idx_tr);
-    ytr = ytr(idx_tr);
-    BOW_xtr = BOW_xtr(idx_tr);
-    indices_tr = indices_tr(idx_tr);
-    
     ntr = length(ytr);
-    nv = length(yv);
     nte= length(yte);
     dim = size(xtr{1},1);
 
@@ -49,10 +38,6 @@ for split = 1:cv_folds
     xtr_center = zeros(dim, ntr);
     for i  = 1:ntr
         xtr_center(:,i) = xtr{i} * BOW_xtr{i}' / sum(BOW_xtr{i});
-    end
-    xv_center = zeros(dim, nv);
-    for i  = 1:nv
-        xv_center(:,i) = xv{i} * BOW_xv{i}' / sum(BOW_xv{i});
     end
     xte_center = zeros(dim, nte);
     for i  = 1:nte
@@ -90,24 +75,13 @@ for split = 1:cv_folds
         A = A - lr_A * dA;
 
         % Compute loss
-        filename = [save_path, dataset,'_', num2str(lambda), '_', num2str(lr_w), '_', num2str(lr_A), '_', num2str(max_iter), '_', num2str(batch), '_', num2str(range), '_', num2str(split), '.mat'];
+        filename = [save_path, dataset,'_SWMD_matrix.mat'];
         if mod(iter, save_frequency) == 0
             save_couter = save_couter + 1;
-            err_v = knn_swmd(xtr, ytr, xv, yv, BOW_xtr, BOW_xv, indices_tr, indices_v, w, lambda, A);
-            err_t = knn_swmd([xtr, xv], [ytr, yv], xte, yte, [BOW_xtr, BOW_xv], BOW_xte, [indices_tr, indices_v], indices_te, w, lambda, A);
-            Err_v(save_couter,:) = err_v;
-            Err_t(save_couter,:) = err_t;
-            w_all{save_couter} = w;
-            A_all{save_couter} = A;
-            save(filename,'Err_v', 'Err_t', 'w_all', 'A_all', 'Ascaled')
+            SWMD_tr = distance_matrix_swmd(xtr, ytr, BOW_xtr, indices_tr, w, lambda, A);
+            SWMD_all = distance_matrix_swmd([xtr, xte], [ytr, yte], [BOW_xtr, BOW_xte], [indices_tr, indices_te], w, lambda, A);
+            save(filename,'SWMD_tr', 'SWMD_all', 'xtr','xte','ytr','yte');
         end
         tIterEnd = toc(tIterStart);
     end
-    
-    tt = toc(tStart);
-
-    err_t_cv = Err_t(Err_v == min(min(Err_v)));
-    T(split) = tt;
-    results_cv(split) = err_t_cv(1);
-    save([save_path, dataset, '_results'], 'results_cv', 'T')
 end
